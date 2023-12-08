31ecb062478c70cfb49e51e40a41f18c2d00bb6d
Jaylen Thomas <jaylenthomas@Jaylens-MacBook-Pro.local>
2023-12-08 11:11:14 -0500

    initial code commit

---
+++ pro-play-pal-rest/src/main/java/com/gamestats/proplaypalrest/ProPlayPalRestApplication.java
@@ -0,0 +1,13 @@
+package com.gamestats.proplaypalrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProPlayPalRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProPlayPalRestApplication.class, args);
+    }
+
+}
---
+++ pro-play-pal-rest/src/test/java/com/gamestats/proplaypalrest/ProPlayPalRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamestats.proplaypalrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProPlayPalRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
