ab63ff40b7dd7502c1877e6f276025e61e0364bb
Jaylen412 <93702389+jaylen412@users.noreply.github.com>
2024-02-19 10:08:19 -0500

    player service class

---
+++ pro-play-pal-rest/src/main/java/com/gamestats/proplaypalrest/service/PlayerService.java
@@ -0,0 +1,9 @@
+package com.gamestats.proplaypalrest.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class PlayerService {
+}
