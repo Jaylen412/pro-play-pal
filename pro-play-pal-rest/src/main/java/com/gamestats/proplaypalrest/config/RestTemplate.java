d958d275d4cddc89aa684ab45b6d0b0977031e34
Jaylen412 <93702389+jaylen412@users.noreply.github.com>
2024-02-23 16:54:13 -0500

    initial team api building

---
+++ pro-play-pal-rest/src/main/java/com/gamestats/proplaypalrest/config/RestTemplate.java
@@ -0,0 +1,8 @@
+package com.gamestats.proplaypalrest.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RestTemplate {
+
+}
--- pro-play-pal-rest/src/main/java/com/gamestats/proplaypalrest/config/SportsDataApiConfig.java
+++ pro-play-pal-rest/src/main/java/com/gamestats/proplaypalrest/config/SportsDataApiConfig.java
@@ -1,8 +1,29 @@
 package com.gamestats.proplaypalrest.config;
 
+import com.gamestats.proplaypalrest.model.Team;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class SportsDataApiConfig {
 
+    private RestTemplate restTemplate;
+    private String apiUrl;
+
+    public SportsDataApiConfig(@Autowired RestTemplate restTemplate,
+                               @Value("${sports-data.urls.teams-basic}") String TeamApiUrl) {
+        this.restTemplate = restTemplate;
+        this.apiUrl = TeamApiUrl;
+    }
+    public List<Team> getTeamData() {
+        List<Team> teamData = new ArrayList<>();
+        ResponseEntity<Team> response = restTemplate.getForEntity(apiUrl, Team.class, teamData);
+        return teamData;
+    }
 }
